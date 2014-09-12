c6ba2b99c27a5163762403dc49484fdb830abb35
Engels <inrod002@gmail.com>
2014-09-12 08:23:26 -0500

    Update mail.py

--- mail.py
+++ mail.py
@@ -8,9 +8,9 @@ import datetime
 class Mailer():
 	def __init__(self):
 		self.eventlist = []
-		self.foldername = None
+		self.foldername = 'C:/ION_Mail'
 		self.event = 0
-		self.timestamp = 1381463561
+		self.timestamp = 1
 		self.datetime = None
 		self.SMTPserver = None
 		self.sender = None
@@ -18,22 +18,28 @@ class Mailer():
 		self.destination = []
 		self.body = ""
 		self.gmt = 0
+		self.config = ''
+		self.config_file = ''
+		self.config_msg = ''
+		self.is_test = False
 
-# sys.argv = [ exe name , param 1 (eventcode), param 2 (timestamp)]
+# sys.argv = [ exe name , param 1 (eventcode), param 2 (timestamp), param 3 (substation config file)]
 	def ParseGetVars(self):
 		self.foldername = os.path.dirname(str(sys.argv[0]))
-		if len(sys.argv)<3:
-			self.event = 0
-			self.timestamp = 1381463561
+		if len(sys.argv)<4:
+			self.is_test = True
 		else:
 			self.event = str(sys.argv[1])
 			self.event = self.event[0:-3]
 			self.timestamp = str(sys.argv[2])
 			self.timestamp = self.timestamp[0:-3]
+			self.config = str(sys.argv[3])
+			self.config_file = self.foldername + '/m_serverconfig_'+self.config+'.txt'
+			self.config_msg = self.foldername + '/m_message_'+self.config+'.txt'
 
 	def OpenConfigFile(self):
 		mailfile = []
-		with open(self.foldername + '/m_serverconfig.txt', 'r') as f:
+		with open(self.config_file, 'r') as f:
 			for line in f.readlines():
 				if line[0:1]!="#":
 					mailfile.append(line[0:-1])
@@ -44,13 +50,10 @@ class Mailer():
 		self.SMTPserver = mailfile[0]
 		self.sender = mailfile[1]
 		self.subject = mailfile[2]
-		if mailfile[3].isdigit() == True:
-			self.gmt = mailfile[3]
-		else:
-			self.gmt=0
+		self.gmt = mailfile[4]
 
 	def OpenMessageFile(self):
-		with open(self.foldername  + '/m_message.txt', 'r') as f:
+		with open(self.config_msg, 'r') as f:
 			for line in f.readlines():
 				self.eventlist.append(line[0:-1])
 
@@ -77,43 +80,36 @@ class Mailer():
 
 	def MainMail(self, test=False):
 		self.ParseGetVars()
-		self.OpenConfigFile()
-		self.OpenMessageFile()
-		self.TimestampConvert()
-		self.SelectEvent(str(self.event), self.eventlist)
-		self.OpenBodyFile()
-		content=self.body
-		content_test = """
-		<html><body>
-		MEDIDOR ION
-		<br><br>
-		*** TESTING MAIL, it Works!! ***
-		<br><br><br>
-		Servicios Avanzados
-		Schneider Electric Peru - 2013
-		</body></html>
-		"""
-		'''
-		print self.body
-		print self.event
-		print self.datetime
-		print self.destination
-		print self.sender
-		print self.SMTPserver
-		print self.eventlist
-		'''
-		if test==False:
-			msg = email.mime.text.MIMEText(content, self.text_subtype)
-		else:
+		if self.is_test == True:
+			content_test = """
+			<html><body>
+			*** TEST MAIL, it Works!! ***
+			<br><br><br>
+			Engels Rodriguez<br>
+			Servicios Avanzados<br>
+			<b>Schneider Electric Peru - 2014</b>
+			</body></html>
+			"""
 			msg = email.mime.text.MIMEText(content_test, self.text_subtype)
+		else:
+			self.OpenConfigFile()
+			self.OpenMessageFile()
+			self.TimestampConvert()
+			self.SelectEvent(str(self.event), self.eventlist)
+			self.OpenBodyFile()
+			msg = email.mime.text.MIMEText(self.body, self.text_subtype)
 		msg['Subject']= self.subject + ' ' + self.event
 		msg['From']   = self.sender
-
 		conn = SMTP(self.SMTPserver)
 		conn.set_debuglevel(False)
 		#conn.login(USERNAME, PASSWORD)
 		conn.sendmail(self.sender, self.destination, msg.as_string())
 		conn.close()
 
-mail = Mailer()
-mail.MainMail()
+try:
+	mail = Mailer()
+	mail.MainMail()
+except Exception,e:
+	f=open('C:/ION_Mail/error.txt','a')
+	f.write(str(e))
+	f.close()
